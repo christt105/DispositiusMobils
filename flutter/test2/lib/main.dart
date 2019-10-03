dede9cecacda08738f4f44bbd6427ac5144b9d3a
Christian Martínez de la Rosa <36189169+christt105@users.noreply.github.com>
2019-10-03 16:48:39 +0200

    semaforo

--- flutter/test2/lib/main.dart
+++ flutter/test2/lib/main.dart
@@ -1,66 +1,89 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  myApp();
+  runApp(App());
 }
 
-void myApp() {
-  String msg = 'pringao';
-  var txt = TextEditingController();
-  txt.text = msg;
-  runApp(
-    MaterialApp(
-      title: 'hello',
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Hola pringao')),
-        body: Column(
-          children: <Widget>[
-            TextField(controller: txt, textAlign: TextAlign.center),
-            Text(msg),
-            Image.asset('assets/hi.jpg'),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50.0),
-                child: Container(
-                  child: Ink(
-                    decoration: ShapeDecoration(
-                        color: Colors.purple, shape: CircleBorder()),
-                    child: Center(
-                        child: IconButton(
-                      color: Colors.red,
-                      icon: Icon(Icons.favorite),
-                      iconSize: 50,
-                      splashColor: Colors.purple,
-                      onPressed: () {
-                        Random r = Random();
-                        int a = r.nextInt(6);
-                        if (a == 0) {
-                          msg = 'sabes';
-                        } else if (a == 1) {
-                          msg = 'que';
-                        } else if (a == 2) {
-                          msg = 'es';
-                        } else if (a == 3) {
-                          msg = 'dificil';
-                        } else if (a == 4) {
-                          msg = 'leer';
-                        } else if (a == 5) {
-                          msg = 'desordenadamente';
-                        }
-                        txt.text = msg;
-                      },
-                    )),
-                  ),
+class App extends StatelessWidget {
+  const App({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width2 = 100;
+    double height2 = 100;
+                return MaterialApp(
+                  home: Scaffold(
+                    appBar: AppBar(
+                      title: Text("HEY"),
+                    ),
+                    //body: ex1(),
+                    body: Center(
+                      child: Container(
+                        width: 150,
+                        height: 350,
+                        color: Colors.black,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,  
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(color:Colors.red,shape:BoxShape.circle),
+                              width: width2,
+                          height: height2,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(color:Colors.orange,shape: BoxShape.circle),          
+                          width: width2,
+                          height: height2,
                 ),
-              ),
-            )
-          ],
+                Container(
+                  decoration: BoxDecoration(color:Colors.green,shape:BoxShape.circle),
+                  width: width2,
+                  height: height2,
+                )
+              ],
+            ),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
+
+  Column ex1() {
+    return Column(
+      children: <Widget>[
+        Banda(20, "20%", Colors.redAccent),
+        Banda(30, "30%", Colors.purple),
+        Banda(50, "50%", Colors.blue)
+      ],
+    );
+  }
+}
+
+class Banda extends StatelessWidget {
+  int flex;
+  String txt;
+  Color color;
+  Banda(this.flex, this.txt, this.color, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: this.flex,
+      child: Container(
+        child: Center(
+          child: Text(
+            this.txt,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        color: this.color,
+      ),
+    );
+  }
 }

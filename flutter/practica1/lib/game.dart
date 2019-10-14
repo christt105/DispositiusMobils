bfd58103fe215859ff33218975553a61965139a7
Christian Martínez <christt105@gmail.com>
2019-10-14 20:50:28 +0200

    Base page created

---
+++ flutter/practica1/lib/game.dart
@@ -0,0 +1,33 @@
+class Game {
+  String name;
+  List<String> consoles;
+  String description;
+  String mainImage;
+  String backgroundImage;
+  String infoImage;
+
+  Game(
+    this.name,
+    this.consoles,
+    this.description,
+    this.mainImage,
+    this.backgroundImage,
+    this.infoImage
+    );
+}
+
+final hk = Game(
+  'Hollow Knight',
+  ['PC', 'PS4', 'NINTENDO SWITCH', 'XBOX ONE'],
+  /*'Forge your own path in Hollow Knight! An epic action adventure through a vast ruined '
+  'kingdom of insects and heroes. Explore twisting caverns, battle tainted creatures and '
+  'befriend bizarre bugs, all in a classic, hand-drawn 2D style.'*/
+
+  'Hollow Knight is a Metroidvania video game developed and published by Team Cherry. '
+  'The game was released for Microsoft Windows, macOS, and Linux in 2017, and for the '
+  'Nintendo Switch, PlayStation 4, Xbox One in 2018. Development was partially funded '
+  'through a Kickstarter crowdfunding campaign, raising over 35,000€ by the end of 2014.',
+  'assets/cover.jpg',
+  'assets/backCover.png',
+  'assets/info.png'
+  );
--- flutter/practica1/lib/main.dart
+++ flutter/practica1/lib/main.dart
@@ -1,26 +1,190 @@
 import 'package:flutter/material.dart';
+import 'game.dart';
+import 'dart:ui'; // para el ImageFilter para crear un efecto de Blur
 
 void main() {
-  runApp(App());
+  runApp(CarInfoApp());
 }
 
-class App extends StatelessWidget {
-  const App({
-    Key key,
-  }) : super(key: key);
-
+class CarInfoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("HEY"),
+      theme: ThemeData(
+        brightness: Brightness.dark,
       ),
-      body: Center(
-          child: Text(
-        "DANTE PEDAZO DE HIJO DE PUTA DONDE COÑO TE HAS IDO",
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
-      )),
-    ));
+      home: GamePage(hk),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  final Game game;
+  const GamePage(this.game);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(game.backgroundImage),
+                        fit: BoxFit.fitWidth),
+                  ),
+                  height: 750,
+                  child: BackdropFilter(
+                    filter:
+                        ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: Container(
+                      width: 200,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(game.mainImage),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            width: 500,
+            height: 350,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "ACTION",
+                    style: TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    game.name,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      height: 20,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 4, right: 4),
+                        child: Text(
+                          "PS4",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 12),
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Container(
+                        height: 20,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 4, right: 4),
+                          child: Text(
+                            "PC",
+                            style: TextStyle(fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(3),
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Text(
+                    game.description,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 55,
+            width: MediaQuery.of(context).size.width,
+            child: FlatButton(
+              onPressed: () {},
+              child: Text("+ ADD GAME"),
+              color: Colors.pink,
+              shape: ContinuousRectangleBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: ShaderMask(
+              shaderCallback: (rect) {
+                return LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black, Colors.transparent],
+                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+              },
+              blendMode: BlendMode.dstIn,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(game.infoImage),
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
